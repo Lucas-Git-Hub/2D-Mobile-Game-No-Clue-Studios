3832ce7155906631a0a88d6e71b3d606c9d1cdcb
Lucas-Git-Hub <lvanwaas@hotmail.com>
2023-09-29 12:33:13 +0200

    Lvl data + scene bugs + lvl bugs fix

--- Assets/Scripts/UI/Map/LevelData.cs
+++ Assets/Scripts/UI/Map/LevelData.cs
@@ -1,29 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class LevelData : MonoBehaviour
-{
-    public GameObject b_Gear;
-    private GameObject s_Gear;
-    private GameObject g_Gear;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(PlayerPrefs.GetInt("Lvl" + 1));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UI;
 
 public class LevelData : MonoBehaviour
